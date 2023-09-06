231ef4f52ba9c415e75608f4b8b7fc468ee7e0eb
Noir_Egoiste <dimon0.0@yandex.ru>
2023-09-06 06:21:54 +0300

    Complite Blog site

---
+++ mini_blog/forms.py
@@ -0,0 +1,57 @@
+from django import forms
+from .models import Post, Category, Comment
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author',
+                  'category', 'body', 'header_image')
+
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control', 'placeholder': 'Enter Title'
+            }),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'elder', 'type': 'hidden'}),
+
+            'category':
+                forms.Select(
+                    choices=Category.objects.all().values_list('name', 'name'
+                                                               ),
+                    attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            # 'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control', 'placeholder': 'Enter Title'
+            }),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            # 'snippet': forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body',)
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control', 'placeholder': 'Enter Name'}),
+            'body': forms.Textarea(attrs={
+                'class': 'form-control', 'placeholder': 'Enter Comment'
+            }),
+        }
